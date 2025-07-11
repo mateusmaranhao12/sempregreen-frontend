7b999198ca4d340d18b44b3ab6d3e3db7e85336b
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-07-11 10:49:41 -0300

    Update api

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  //baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://sempregreen-backend.onrender.com/api',
 })
 
 export default api
